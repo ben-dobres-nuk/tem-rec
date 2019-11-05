688a5f68d454fcad58a209bd9b1f54e077bd8323
ben-dobres-nuk <ben.dobres@news.co.uk>
2019-11-05 13:13:57 +0000

    foo

--- article_index/xgb_test.py
+++ article_index/xgb_test.py
@@ -10,28 +10,36 @@ from glob import glob
 from copy import deepcopy
 import yaml
 
+yaml_feature_names = [
+    "proportion_internal_index_subscriber_read",
+    "proportion_internal_article_subscriber_read",
+    "proportion_direct_subscriber_read",
+    "proportion_desktop_web_subscriber_read",
+    "proportion_mobile_web_subscriber_read",
+    "proportion_other_web_subscriber_read", "proportion_apps_subscriber_read",
+    "unique_commenters", "comments", "save_adds_subscriber_read",
+    "avg_session_hit_count_subscriber_read",
+    "avg_session_read_count_subscriber_read", "words", "sentences",
+    "cs_section_category", "section_category", "num_characters", "age_hours",
+    "day_of_week"
+]
 
-yaml_feature_names = ["proportion_internal_index_subscriber_read",
-"proportion_internal_article_subscriber_read",
-"proportion_direct_subscriber_read",
-"proportion_desktop_web_subscriber_read",
-"proportion_mobile_web_subscriber_read",
-"proportion_other_web_subscriber_read",
-"proportion_apps_subscriber_read",
-"unique_commenters",
-"comments",
-"save_adds_subscriber_read",
-"avg_session_hit_count_subscriber_read",
-"avg_session_read_count_subscriber_read",
-"words",
-"sentences",
-"cs_section_category",
-"section_category",
-"num_characters",
-"age_hours",
-"day_of_week"]
+xgb = XGBRegressor(
+    max_depth=3,
+    seed=2012,
+    learning_rate=0.1,
+    colsample_bylevel=0.9,
+    subsample=0.7,
+    n_estimators=1000,
+    silent=True,
+    objective='reg:linear',
+    n_jobs=-1,
+    min_child_weight=5,
+    reg_lambda=3,
+    gamma=2,
+    colsample_bytree=0.9)
 
-all_data = pd.read_pickle('article_index/data/all_data_reindex.pkl')
+all_data = pd.read_pickle('all_data_reindex.pkl')
 
 data_sample = all_data.sample(frac=0.1)
 
