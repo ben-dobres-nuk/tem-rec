04b8d820707de84403bb3c6debe8bf8af8727a87
ben-dobres-nuk <ben.dobres@news.co.uk>
2019-11-11 13:48:27 +0000

    loggigng and args

--- article_index/xgb_test.py
+++ article_index/xgb_test.py
@@ -1,10 +1,35 @@
 import pandas as pd
 import numpy as np
-# import random
+import random
 
 from xgboost import XGBRegressor
+# from sklearn.kernel_ridge import KernelRidge
+# from sklearn.svm import SVR, LinearSVR
+from sklearn.model_selection import cross_validate
 
-n_seeds = 30
+import argparse
+import logging
+import warnings
+
+logging.basicConfig(
+    filename='seed_test_' + str(random.getrandbits(20)) + '.log',
+    level=logging.DEBUG)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '-s',
+    '--sample',
+    type=float,
+    default=0.001,
+    help="sample size as a fraction")
+
+args = parser.parse_args()
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# removing unique commenters as a feature gives us a small improvement
+# on seed variance with almost no impact on accuracy
 
 yaml_feature_names = [
     "proportion_internal_index_subscriber_read",
@@ -12,68 +37,114 @@ yaml_feature_names = [
     "proportion_direct_subscriber_read",
     "proportion_desktop_web_subscriber_read",
     "proportion_mobile_web_subscriber_read",
-    "proportion_other_web_subscriber_read", "proportion_apps_subscriber_read",
-    "unique_commenters", "comments", "save_adds_subscriber_read",
+    "proportion_other_web_subscriber_read",
+    "proportion_apps_subscriber_read",
+
+    # "unique_commenters",
+    "comments",
+    "save_adds_subscriber_read",
     "avg_session_hit_count_subscriber_read",
-    "avg_session_read_count_subscriber_read", "words", "sentences",
-    "cs_section_category", "section_category", "num_characters", "age_hours",
+    "avg_session_read_count_subscriber_read",
+    "words",
+    "sentences",
+    "cs_section_category",
+    "section_category",
+    "num_characters",
+    "age_hours",
     "day_of_week"
 ]
 
+# sample_frac = 0.001
 all_data = pd.read_pickle('all_data_reindex.pkl')
-data_sample = all_data.sample(frac=0.1)
+data_sample = all_data.sample(frac=args.sample, random_state=801)
 
 X = data_sample[yaml_feature_names]
 y = data_sample['avg_dwell_time_subscriber_read']
-print("X shape:")
-print(X.shape)
-print("      ")
 
 # generate seeds for random sample later
 # seed2 = random.sample(range(1, X.shape - 1), 1000)
 
+# booster: gbtree, gblinear or dart?
 
-def trial_seed(n):
-    xgb = XGBRegressor(
-        max_depth=3,
-        seed=n,
-        learning_rate=0.1,
-        colsample_bylevel=0.9,
-        subsample=0.7,
-        n_estimators=1000,
-        silent=True,
-        objective='reg:linear',
-        n_jobs=-1,
-        min_child_weight=5,
-        reg_lambda=3,
-        gamma=2,
-        colsample_bytree=0.9)
+xgb_params = {
+    'max_depth': 2,
+    'learning_rate': 0.1,
+    'colsample_bylevel': 0.9,
+    'subsample': 0.7,
+    'n_estimators': 1000,
+    'silent': True,
+    # switched reg:linear for reg:squarederror
+    'objective': 'reg:squarederror',
+    'n_jobs': -1,
+    'min_child_weight': 5,
+    'reg_lambda': 3,
+    'gamma': 10,
+    'colsample_bytree': 0.9
+}
 
-    xgb.fit(X, y)
-    score = xgb.score(X, y)
-    text_output = "Score for seed {} is {}".format(n, score)
-    print(text_output)
+# linsvr_params = {
+#     'C': 1.0,
+#     'dual': True,
+#     'epsilon': 0.0,
+#     'fit_intercept': True,
+#     'intercept_scaling': 1.0,
+#     'loss': 'epsilon_insensitive',
+#     'max_iter': 1000000
+# }
+# svr_params = {'kernel': 'rbf', 'gamma': '0.1'}
+# krr_params = {'kernel': 'rbf', 'gamma': '0.1'}
 
-    return (xgb.predict(X))
+
+def trial_seed(model, params, n, seed_param):
+
+    params[seed_param] = n
+
+    model = model(**params)
+
+    model.fit(X, y)
+    # cross validate
+    cv = cross_validate(model, X, y, cv=2)
+
+    return ((model.predict(X), cv['test_score']))
 
 
-def make_seed_dataset(n_seeds):
-    trials = []
+def make_seed_dataset(model, params, n_seeds, seed_param):
+    logging.info("model: {}".format(str(model)))
+    logging.info("sample size: {}".format(len(data_sample.index)))
+    logging.info(yaml_feature_names)
+    logging.info(params)
+    logging.info("Number of seeds: {}".format(n_seeds))
+    trials = {"seeds": [], "test_score": []}
     for n in range(1, n_seeds):
-        trials.append(trial_seed(n))
+        trial = trial_seed(model, params, n, seed_param)
+        trials["seeds"].append(trial[0])
+        trials["test_score"].append(trial[1][0])
+
+    mean_cv_score = np.mean(trials["test_score"])
 
     df = pd.DataFrame(
-        np.vstack(trials),
+        np.vstack(trials["seeds"]),
         columns=["row" + str(x) for x in range(1, y.shape[0] + 1)])
-    print("Creating seed dataset of shape {}".format(df.shape))
+    logging.info("Created seed dataset of shape {}".format(df.shape))
+    # Create summary dataframe with a row for each observation
     stats = pd.DataFrame()
+    # mean, standard deviation and variance for each observation across seeds:
     stats["mean"] = df.mean()
     stats["st_dev"] = df.std()
     stats["var"] = df.var()
     print("Creating summary dataset of shape {}".format(stats.shape))
     df.to_csv("raw_seed_datset.csv")
     stats.to_csv("seed_stats.csv")
+    prop_high_stdev = sum(stats["st_dev"] > 2) / len(stats["st_dev"])
+
+    logging.info("Proportion of obervations with Standard Deviation > 2: {}".
+                 format(prop_high_stdev))
+
+    logging.info("Mean cross-validation score: {}".format(mean_cv_score))
 
 
 if __name__ == '__main__':
-    make_seed_dataset(n_seeds)
+    n_seeds = 30
+
+    make_seed_dataset(XGBRegressor, xgb_params, n_seeds, seed_param="seed")
+    logging.info("end of seed test")
